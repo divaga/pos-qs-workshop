22391e5040901f0b95bb23925b04afe51d5cdb27
Ivan <13087388+divaga@users.noreply.github.com>
2021-11-15 20:19:23 +0700

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,17 +1,17 @@
 CREATE TABLE public.kota (
     provinsi text,
     nama_kota text,
-    latitude numeric(5,2),
-    longitude numeric(5,2)
+    latitude numeric(8,5),
+    longitude numeric(8,5)
 );
 
 CREATE TABLE public.realisasi (
 nama_kota text,
 nama_penerima text,
-jumlah_realisasi numeric(5,2)
+jumlah_realisasi integer
 );
 
 CREATE TABLE public.alokasi (
 nama_kota text,
-jumlah_alokasi numeric(5,2)
+jumlah_alokasi integer
 );
